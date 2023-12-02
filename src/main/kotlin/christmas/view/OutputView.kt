bff668a11d6373a4a6737c8d54800225962f600f
altpfwlzh <altpfwlzh@gmail.com>
2023-12-02 20:18:57 +0900

    feat : 출력해야 하는 멘트 선언 및 정리

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -6,4 +6,26 @@ import christmas.constants.Strings
 class OutputView() {
     private val strings: Strings = Strings
     private val errorMessage: ErrorMessage = ErrorMessage
+
+    fun hello() = println(strings.OUTPUT_HELLO)
+
+    fun visitDate() = println(strings.OUTPUT_VISIT_DATE)
+
+    fun eventPreview() = println(strings.OUTPUT_EVENT_PREVIEW)
+
+    fun order() = println(strings.OUTPUT_ORDER)
+
+    fun orderMenu() = println(strings.OUTPUT_ORDER_MENU)
+
+    fun totalAmountBeforeDiscount() = println(strings.OUTPUT_TOTAL_AMOUNT_BEFORE_DISCOUNT)
+
+    fun giftDetail() = println(strings.OUTPUT_GIFT_DETAIL)
+
+    fun benefitDetail() = println(strings.OUTPUT_BENEFIT_DETAIL)
+
+    fun totalDiscountAmount() = println(strings.OUTPUT_TOTAL_DISCOUNT_AMOUNT)
+
+    fun totalAmountAfterDiscount() = println(strings.OUTPUT_TOTAL_AMOUNT_AFTER_DISCOUNT)
+
+    fun badgeDetail() = println(strings.OUTPUT_BADGE_DETAIL)
 }
